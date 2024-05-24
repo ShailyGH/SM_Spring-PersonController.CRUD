10f25dabfe80c2d189e2f93a27dfd66cc76533cf
Shaily Modi <shailykunal@gmail.com>
2024-05-24 11:42:24 -0400

    Finished.

--- src/main/java/io/zipcoder/crudapp/PersonController.java
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -3,18 +3,47 @@ package io.zipcoder.crudapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+
 @RestController
+@RequestMapping("/people")
 public class PersonController {
 
     @Autowired
     private PersonRepository personRepository;
 
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @PostMapping("/people")
     public ResponseEntity<Person> createPerson(@RequestBody Person person) {
         return new ResponseEntity<>(personRepository.save(person), HttpStatus.CREATED);
+    }
+    @GetMapping("/people/{id}")
+    public ResponseEntity<Person> getPerson(@PathVariable int id) {
+        return new ResponseEntity<>(personRepository.findOne(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/people")
+    public ResponseEntity<Iterable<Person>> getPersonList() {
+        return new ResponseEntity<>(personRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping("/people/{id}")
+    public ResponseEntity<Person> updatePerson(@RequestBody Person p, @PathVariable int id) {
+        if (p.getId() != null) { //updating
+            return new ResponseEntity<>(personRepository.save(p), HttpStatus.OK);
+        } else {
+            return createPerson(p);
+        }
+    }
+
+    @DeleteMapping("/people/{id}")
+    public ResponseEntity deletePerson(@PathVariable int id) {
+        personRepository.delete(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/io/zipcoder/crudapp/PersonRepository.java
+++ src/main/java/io/zipcoder/crudapp/PersonRepository.java
@@ -2,5 +2,7 @@ package io.zipcoder.crudapp;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends CrudRepository<Person,Integer> {
-}
+   }
